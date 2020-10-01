edf1fdf8e8df773d153c3197d56458d1b1973fde
tomasmunozj <69277971+tomasmunozj@users.noreply.github.com>
2020-10-01 20:51:14 -0300

    Add files via upload

--- barra.py
+++ barra.py
@@ -45,16 +45,31 @@ class Barra(object):
 
 
 
-
-
-
-
-
     def obtener_rigidez(self, ret):
         """Devuelve la rigidez ke del elemento. Arreglo numpy de (4x4)
         ret: instancia de objeto tipo reticulado
         """
+        L = self.calcular_largo(ret)
+        A = self.calcular_area()
         
+        k = self.E * A / L 
+        
+        
+        xi=ret.xyz[self.ni,0]
+        yi=ret.xyz[self.ni,1]
+        
+        xj=ret.xyz[self.nj,0]
+        yj=ret.xyz[self.nj,1]
+        
+        #Segun el video T teta es delta definido asi:
+        menos_cos = (xi-xj) / L
+        menos_sen = (yi-yj) / L
+        cos       = (xj-xi) / L
+        sen       = (yj-yi) / L
+
+        T0= np.array([[menos_cos], [menos_sen], [cos], [sen]])   
+        
+        ke = T0 @ T0.T * k
         #implementar
 
         return ke
@@ -63,7 +78,9 @@ class Barra(object):
         """Devuelve el vector de cargas nodales fe del elemento. Vector numpy de (4x1)
         ret: instancia de objeto tipo reticulado
         """
-
+        W = self.calcular_peso(ret)
+        
+        fe = np.array([[0],[-1],[0],[-1]]) * (W / 2)
         #Implementar
 
         return fe
@@ -73,11 +90,49 @@ class Barra(object):
         """Devuelve la fuerza se que debe resistir la barra. Un escalar tipo double. 
         ret: instancia de objeto tipo reticulado
         """
-
         #Implementar
+        A=self.calcular_area()
+        L=self.calcular_largo(ret)        
+        ni=self.ni
+        nj=self.nj
+        ue=np.array([ret.u[ni*2], ret.u[((2*ni)+1)], ret.u[2*nj], ret.u[((2*nj)+1)]])       
+        xi=ret.xyz[ni,0]
+        yi=ret.xyz[ni,1]
+        
+        xj=ret.xyz[nj,0]
+        yj=ret.xyz[nj,1]
+        
+        menos_cos = (xi-xj) / L
+        menos_sen = (yi-yj) / L
+        cos       = (xj-xi) / L
+        sen       = (yj-yi) / L
 
+        T0= np.array([[menos_cos], [menos_sen], [cos], [sen]])
+        delta = T0.T @ ue
+        
+        se = ((A*self.E) / L )* delta
 
         return se
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
